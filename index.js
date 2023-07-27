c22e3a107e952e991852dc5fc4a44dab84e3c1c6
P Sudip Kumar Prusty <sudipprustysudip1905@gmail.com>
2023-07-27 18:11:49 +0530

    Slider done. recommended section on-progress.

--- index.js
+++ index.js
@@ -1,77 +1,92 @@
 let movies = [
     {
-        name: "falcon and the winter soldier",
-        des:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam exc",
-        image: "images/slider 2. png"
+        name: 'falcon and the winter soldier',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 2.PNG'
     },
     {
-        name: "loki",
-        des:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam exce",
-        image: "Images/slider 1. png"
+        name: 'loki',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 1.PNG'
     },
     {
-        name: "wanda vision",
-        des:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam exce",
-        image: "images/slider 3.png"
+        name: 'wanda vision',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 3.PNG'
     },
     {
-        name: "raya and the last dragon",
-        des:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam exce",
-        image: "images/slider 4.png"
+        name: 'raya and the last dragon',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 4.PNG'
     },
     {
-        name: "luca",
-        des:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam exce",
-        image: "images/slider 5.png"
+        name: 'luca',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 5.PNG'
     }
-];
+]
 
-const carousel=document.querySelector('.carousel');
+let carousel = document.querySelector('.carousel');
 let sliders = [];
 
-let slideIndex = 0;     //track the current slide
+let slideIndex = 0;
 
-const createSlide = () => {
+const createSlide = () =>{
     if(slideIndex >= movies.length){
-        slideIndex = 0;
+        slideIndex=0;
     }
 
-    //create DOM elements
     let slide = document.createElement('div');
-    var imgElement = document.createElement('img');
+    let imgElement = document.createElement('img');
     let content = document.createElement('div');
     let h1 = document.createElement('h1');
     let p = document.createElement('p');
 
-    //attaching all elements
     imgElement.appendChild(document.createTextNode(''));
     h1.appendChild(document.createTextNode(movies[slideIndex].name));
     p.appendChild(document.createTextNode(movies[slideIndex].des));
     content.appendChild(h1);
     content.appendChild(p);
+    slide.appendChild(content);
     slide.appendChild(imgElement);
     carousel.appendChild(slide);
 
-    //getting up the images
-    imgElement.src=movies[slideIndex].image;
-    slideIndex++
 
-    //setting elements classnames
-    slide.className="slider";
-    content.className='slide-content';
-    h1.className='movie-title';
-    p.className='movie-des';
+    imgElement.src = movies[slideIndex].image;
+    slideIndex++;
 
+    slide.className = 'slider';
+    content.className = 'slider-content';
+    h1.className = 'movie-title';
+    p.className = 'movie-desc';
+    
     sliders.push(slide);
 
-    if(slide.length){
-        sliders[0].style.marginLeft = 'calc(-${100 * (sliders.length - 2)}% - ${
-            30 * (sliders.length - 2)
-        }px)';
-    }
+    if(sliders.length){
+        sliders[0].style.marginLeft =  `calc(-${100 * (sliders.length - 2)}% - ${30 *(sliders.length - 2)}px)`; 
+    }  
+}
+
+for(let i = 0; i < 6; i++){
+    createSlide();
 };
+
+setInterval(()=>{
+    createSlide();
+},3000);
+
+//video cards
+const videoCards = [...document.querySelectorAll('.video-card')];
+
+videoCards.forEach(item => {
+    item.addEventListener('mouseover',() => {
+        let video=item.children[1];
+        video.play();
+    })
+    item.addEventListener('mouseleave',() => {
+        let video=item.children[1];
+        video.pause();
+    })
+})
+
+//card-sliders
